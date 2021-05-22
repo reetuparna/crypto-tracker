5cc7ec35a4a03dbdea46c24782f69eddaf1a2d9f
Reetuparna Mukherjee <reetuparna.1988@gmail.com>
2021-05-22 23:18:13 +0530

    panel css

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './panel.css';
+import {GrNext} from "react-icons/gr";
 /**
 * @author
 * @function Panel
@@ -10,8 +11,9 @@ import './panel.css';
 const Panel = (props) => {
   const dispatch = useDispatch();
   const newsApiKey = useSelector(state => state.newsApiKey);
+  
   useEffect(() => {
-    axios.get(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=${newsApiKey}`).then((response)=>{
+    axios.get(`https://newsapi.org/v2/everything?q=crypto&pageSize=7&apiKey=${newsApiKey}`).then((response)=>{
       dispatch({type: 'SET_NEWS_DATA', value: response.data.articles})
     })
   }, []);
@@ -19,17 +21,20 @@ const Panel = (props) => {
   const newsDataArray = useSelector(state => state.newsDataArray)
   return(
     <div className="panel">
-      <h3 className='panel-head'>Trending</h3>
+      {/* <h3 className='panel-head'>Trending</h3> */}
       <div className='news-list'>
           { newsDataArray.length > 0 &&
             newsDataArray.map((news)=>{
-              debugger;
               return (
-              <div className='news-item'>
-                {news.title}
-                <br/>
-                <small class="author">{`- ${news.source.name}`}</small>
-              </div>
+                <div className='news-item' onClick={() => window.open(news.url)}>
+                  <div className='news-text'>
+                    <div>{news.title}</div>
+                    <div className="source-name">{news.source.name.toUpperCase()}</div>
+                  </div>
+                  <div className='news-src-link'>
+                    <GrNext />
+                  </div>
+                </div>
               )
             })
           }
